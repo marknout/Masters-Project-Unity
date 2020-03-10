df4324f4fa3462a2713b97c40551b10bfbcdc0e4
Mark Outridge <maou1871@edu.kadk.dk>
2020-03-10 17:25:16 +0100

    Perspective camera updates

--- Masters Project Mobile Setup/Assets/PerspectivePan.cs
+++ Masters Project Mobile Setup/Assets/PerspectivePan.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PerspectivePan : MonoBehaviour
 {
+    private Vector3 touchStart;
+    public Camera cam; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+    }
+    private Vector3 GetWorldPosition (){
+
     }
 }
