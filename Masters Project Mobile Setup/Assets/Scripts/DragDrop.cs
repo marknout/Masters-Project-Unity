585f8887fc9035772c6503199d41b13213666d69
Mark Outridge <maou1871@edu.kadk.dk>
2020-04-16 17:28:48 +0200

    select and connect scene setup

--- Masters Project Mobile Setup/Assets/Scripts/DragDrop.cs
+++ Masters Project Mobile Setup/Assets/Scripts/DragDrop.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+   
     void Update()
     {
-        
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+        }
     }
+
+    private void OnMouseDown()
+    {
+        if(Input.GetMouseButton(0)) 
+        {
+
+        Vector3 mousePos;
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        //startPosX = mousePos.x - this.transform.localPosition.x;
+        //startPosY = mousePos.y - this.transform.localPosition.y;
+
+        isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+
+
 }
---
+++ Masters Project Mobile Setup/Assets/Scripts/Slots.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Slots : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+
+            return null;
+        }
+    }
+
+   
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandler.item.transform.SetParent(transform);
+        }
+        else
+        {
+            Transform aux = DragHandler.item.transform.parent;
+            DragHandler.item.transform.SetParent(transform);
+            item.transform.SetParent(aux);
+
+        }
+    }
+
+}
